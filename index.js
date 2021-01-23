0db3e308eb88b50930e852d2da9d6474910f4576
nickd <nickdiaz58@gmail.com>
2021-01-23 14:36:15 -0500

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,58 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(values) {
+    let record = {
+        firstName: values[0],
+        familyName: values[1],
+        title: values[2],
+        payPerHour: values[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return record
+}
+
+function createEmployeeRecords (records) {
+    return records.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(date) {
+    let timeIn = {
+        type: "TimeIn",
+        hour: parseInt(date.split(' ')[1]),
+        date: date.split(' ')[0]
+    }
+    this.timeInEvents.push(timeIn)
+    return this
+}
+
+function createTimeOutEvent(date) {
+    let timeOut = {
+        type: "TimeOut",
+        hour: parseInt(date.split(' ')[1]),
+        date: date.split(' ')[0]
+    }
+    this.timeOutEvents.push(timeOut)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(event => event['date'] === date)
+    let timeOut = this.timeOutEvents.find(event => event['date'] === date)
+    if (timeIn && timeOut) {
+        let hoursWorked = (timeOut['hour'] - timeIn['hour']) / 100
+        return hoursWorked
+    } else {
+        return 0
+    }
+}
+
+function wagesEarnedOnDate(date) {
+    let pay = (hoursWorkedOnDate.call(this, date) * this.payPerHour)
+    return pay
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +71,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    let result = srcArray.find(emp => emp.firstName === firstName)
+    return result
+}
+
+function calculatePayroll(records) {
+    let total = records.reduce(function(base, emp){return base + allWagesFor.call(emp)}, 0)
+    return total
 }
